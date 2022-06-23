1712f891943db67e00815152c841ae622064a45b
Kevin Zhang <kzkevin123@gmail.com>
2022-06-23 17:41:49 -0400

    make routes for new page

--- app.js
+++ app.js
@@ -1,23 +1,26 @@
-const express = require('express');
-const morgan = require('morgan');
-const { db, Page, User } = require('./models');
+const express = require("express");
+const morgan = require("morgan");
+const { db, Page, User } = require("./models");
+const wikiRouter = require("./routes/wiki");
+const userRouter = require("./routes/users");
 
 const app = express();
 
-app.use(morgan('dev'));
-app.use(express.static(__dirname + '/public'));
+app.use(morgan("dev"));
+app.use(express.static(__dirname + "/public"));
 
+app.use("/wiki", wikiRouter);
+app.use("/users", userRouter);
 //app.use(express.urlencoded);
 
-app.get('/', (req,res) => {
-    res.send('hello');
+app.get("/", (req, res) => {
+  res.send("hello");
 });
 
-db.authenticate()
-  .then(() => {
-    console.log('connected to the database');
-  });
-  
+db.authenticate().then(() => {
+  console.log("connected to the database");
+});
+
 // const syncPage = async () => {
 //     await Page.sync();
 //     console.log('page sync done');
@@ -31,13 +34,13 @@ db.authenticate()
 // syncUser();
 
 const syncdb = async () => {
-    await db.sync(); // if model defnition change use db.sync({force: true})
-    console.log('db sync done');
+  await db.sync(); // if model defnition change use db.sync({force: true})
+  console.log("db sync done");
 };
 syncdb();
 
 const PORT = 8080;
 
 app.listen(PORT, () => {
-    console.log(`App running on http://localhost:${PORT}`);
+  console.log(`App running on http://localhost:${PORT}`);
 });
