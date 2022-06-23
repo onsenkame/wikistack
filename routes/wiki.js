d12f6649dd012cb5a342ca5d9abefb1dc5194b93
Kevin Zhang <kzkevin123@gmail.com>
2022-06-23 18:13:20 -0400

    add a page

--- routes/wiki.js
+++ routes/wiki.js
@@ -1,13 +1,15 @@
 const express = require("express");
 const router = express.Router();
+const { addPage } = require("../views/addPage");
+
 router.get("/", (req, res, next) => {
   res.send("retrieve all wiki pages");
 });
 router.post("/", (req, res, next) => {
   res.send("submit new page to the database");
 });
-router.get("/add", (req, res, next) => {
-  res.send("retrieve the add a page form");
+router.get("/add", (req, res) => {
+  res.send(addPage());
 });
 
 module.exports = router;
